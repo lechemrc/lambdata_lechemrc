56965680a4d83ca44e315427da70153bc70a61c8
lechemrc <roblecheminant@gmail.com>
2019-11-07 21:15:39 -0700

    finalizing push

--- lambdata_lechemrc/__init__.py
+++ lambdata_lechemrc/__init__.py
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-__version__ = "0.0.7"
-=======
-__version__ = "0.0.5"
->>>>>>> 5c384d8eb18ed8d933354ce434f3f1e91b5d9ef9
+__version__ = "0.0.9"
 my_str = "this is my_str from __init__.py"
